aecb98a539e6e28633c8f47966b32ed5ca120cae
Alexey.Ivanov1981 <lelikus81@yandex.ru>
2022-12-27 08:22:48 +0300

    create colection in main

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,12 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+
         List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
         List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+
         Collection<Person> persons = new ArrayList<>();
+
         for (int i = 0; i < 10_000_000; i++) {
             persons.add(new Person(
                     names.get(new Random().nextInt(names.size())),
@@ -14,5 +17,7 @@ public class Main {
                     Education.values()[new Random().nextInt(Education.values().length)])
             );
         }
+
+        System.out.println(persons);
     }
 }
